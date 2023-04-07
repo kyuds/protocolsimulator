1321b6614d65b45c0a9954e7ca22e65b02c28544
Daniel Shin <kyuseung1016@gmail.com>
2023-04-07 14:54:37 -0700

    statcollector class setup

--- tmp/statcollector.py
+++ tmp/statcollector.py
@@ -1,39 +1,5 @@
 # Class for Collecting Various Statistics
 
-"""
-Data to collect:
-- Node State: Percentage Full
-"""
-
 class StatCollector:
-    def __init__(self, latency):
-        self.latency = latency
+    def __init__(self):
         self.data = []
-    
-    def aggregate(self):
-        totalStats = {}
-
-        for key in self.data[0].keys():
-            st = 0
-            for d in self.data:
-                st += d[key]
-            totalStats[key] = st
-
-        return totalStats
-
-    def logStats(self):
-        print("Results:")
-        print(self.data)
-    
-    def addNewEntry(self):
-        self.data.append({
-            "nodeStates": [],
-            "spillRestore": [],
-            # add more fields here. 
-        })
-    
-    def addNodeState(self, id, fp):
-        self.data[-1]["nodeStates"].append((id, fp))
-    
-    def addRestoreData(self, id, n):
-        self.data[-1]["spillRestore"].append((id, n))
